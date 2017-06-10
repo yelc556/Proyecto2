50313ab37d648b3557ebca9b0ee6d6a295057c87
yelc-lopez <yelc556@gmail.com>
2017-06-09 18:44:35 -0600

    mensaje del comini

--- ProyectoDePrueba/src/proyectodeprueba/ProyectoDePrueba.java
+++ ProyectoDePrueba/src/proyectodeprueba/ProyectoDePrueba.java
@@ -16,6 +16,7 @@ public class ProyectoDePrueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola mundo");
     }
     
 }
